84024ac46c8dd1d4796ed33e6448faa032261c62
cussingsh4d0w <cussingsh4d0w@gmail.com>
2021-04-04 12:47:34 -0700

    finalized index, script, and style files

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function compute(){
-    var principal = document.getElementById("principal").value;
-    if(principal.value<=0){
-        alert("Enter a positive number");
-        fname.focus();
-        return false;
-    }
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate/100;
-    var end_year = 2021 + years;
-
-    document.getElementById("result").innerHTML = "Test"
-    document.getElementById("result").innerHTML = "If you deposit " + <mark>principal</mark> + "/n";
-    document.write("at an interest rate of " + <mark>rate</mark> + "/n");
-    document.write("You will receive an amount of " + <mark>principal + interest</mark> + "/n");
-    document.write("in the year " + <mark>end_year</mark> + "/n");
-}
 function show_value(x){
     document.getElementById("slider_value").innerHTML=x;
 }
-        
+function compute(){
+    var principal = document.getElementById("principal");
+    //check that user enters a positive number
+    if(principal.value<=0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    var rate = document.getElementById("rate");
+    var years = document.getElementById("years"); 
+    var interest = Number(principal.value) * Number(years.value) * Number(rate.value) / 100;
+    //var total = Number(principal.value) + interest;
+    var end = Number(years.value) + 2021;
+
+    document.getElementById("result1").innerHTML = "If you deposit " + "<span class='highlight'>" + principal.value + "</span>" + ",";
+    document.getElementById("result2").innerHTML = "at an interest rate of " + "<span class='highlight'>" + rate.value + "%" + "</span>" + ".";
+    document.getElementById("result3").innerHTML = "You will receive " + "<span class='highlight'>" + interest + "</span>"+ ",";
+    document.getElementById("result4").innerHTML = "in the year " + "<span class='highlight'>" + end + "</span>" + ".";
+}
